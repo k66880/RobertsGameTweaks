607ac0f723daab6d8d7a09ca9ba436b7e9421ae1
RobertWu <k66880@qq.com>
2025-08-03 16:30:09 +0800

    农田保护

---
+++ src/main/java/com/robertsworks/robertsgametweaks/FarmlandProtection/FarmlandProtectionHandler.java
@@ -0,0 +1,21 @@
+package com.robertsworks.robertsgametweaks.FarmlandProtection;
+
+import com.robertsworks.robertsgametweaks.RobertsGameTweaksMod;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.level.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = RobertsGameTweaksMod.MODID)
+public class FarmlandProtectionHandler {
+    @SubscribeEvent
+    public static void onFarmlandTrample(BlockEvent.FarmlandTrampleEvent event) {
+        Entity entity = event.getEntity();
+        // 仅当实体是玩家时取消践踏事件
+        if (entity instanceof Player) {
+            event.setCanceled(true);
+        }
+    }
+}
